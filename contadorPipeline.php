2e589f08b8ce9ff0013718e64ccb8b92e0447ffd
DenisZwirtes <zwirtesdenis@gmail.com>
2023-03-23 14:35:02 -0300

    função simples para melhor manipular pipelines

---
+++ contadorPipeline.php
@@ -0,0 +1,25 @@
+<?php
+
+$pipelineString = '';
+function contadorPipeline($pipelineString)
+{
+    $pipelineArray = explode('|', $pipelineString, -1);
+
+    //$pipelineArray[14] = '1';
+    foreach ($pipelineArray as $indice => $valor) {
+        $indice++;
+        echo "coluna $indice = $valor  \n";
+    }
+    return $pipelineArray;
+}
+function convPipeString($pipelineArray)
+{
+    $pipelineString = implode("|", $pipelineArray);
+    echo "\n $pipelineString";
+    return $pipelineString;
+}
+
+
+
+$pipelineArray = contadorPipeline($pipelineString);
+$pipelineString = convPipeString($pipelineArray);
